94c19b116bff28a919443026e6479a17ac9716aa
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2020-07-27 13:19:18 +0530

    chore(spring-initializr): create boilerplate with spring-web

---
+++ src/test/java/net/aniruddha/crudspringboot/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.aniruddha.crudspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
